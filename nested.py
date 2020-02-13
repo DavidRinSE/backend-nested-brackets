31eb3198067067f720b81d67f2e2502ec39db8ae
David Richardson <DavidRichardsonIN@gmail.com>
2020-02-13 09:26:35 -0500

    Revert "finished assignment"

--- nested.py
+++ nested.py
@@ -7,45 +7,16 @@ __author__ = "???"
 
 import sys
 
-def is_nested(line):
-    brackets = {
-        "(":["open"], ")":["close", "("], 
-        "[":["open"], "]":["close", "["], 
-        "{":["open"], "}":["close", "{"], 
-        "<":["open"], ">":["close", "<"], 
-    }
-    
-    stack = []
-    tokens = 0
-    
-    for i, c in enumerate(line, start=0):
-        if line[i:i+2] == "(*":
-            stack.append("(*")
-        elif line[i-1:i+1] == "*)" and line[i-2] != "(":
-            if stack[-1] == "(*":stack.pop()
-            else: return "NO {}".format(tokens)
-        elif c in brackets:
-            tokens += 1
-            if brackets[c][0] == "open": stack.append(c)
-            elif stack[-1] == brackets[c][1]: stack.pop()
-            else: return "NO {}".format(tokens)
-        else: tokens += 1
-    if len(stack) != 0: return "NO {}".format(tokens)
-    return "YES"
 
-def start_nested(filename):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            print(is_nested(line))
+def is_nested(line):
+    """Validate a single input line for correct nesting"""
+    pass
+
 
 def main(args):
-    if len(sys.argv) != 2:
-        print ('usage: python nested.py file')
-        sys.exit(1)
-
-    filename = sys.argv[1]
-    start_nested(filename)
+    """Open the input file and call `is_nested()` for each line"""
+    # Results: print to console and also write to output file
+    pass
 
 
 if __name__ == '__main__':
